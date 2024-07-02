f60dfe422b0d376f0335db427de1826b632bb7f2
da4do0 <79230118+da4do0@users.noreply.github.com>
2024-07-02 08:20:08 +0200

    Updates

---
+++ static/js/952.eeb33b1b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkparking_police=self.webpackChunkparking_police||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.eeb33b1b.chunk.js.map
